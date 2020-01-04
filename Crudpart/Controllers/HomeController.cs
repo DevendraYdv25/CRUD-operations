04d0e802c495a3a76319e64ae694f0bfd719adfe
Devenda Yadav <devendrayadav7676@gmail.com>
2020-01-05 02:36:33 +0530

    Crud Operations In simple way

---
+++ Crudpart/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyAppModels;
+using MyAppDb.DbOperatuon;
+
+namespace Crudpart.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeRepository repository = null;
+        public HomeController()
+        {
+            repository = new EmployeeRepository();
+        }
+        //Insert Action methods 
+        //      insert All The All The Records At time
+        // GET: Home
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel model)
+        
+{
+            if (ModelState.IsValid)
+            {
+                int id = repository.AddEmployee(model);
+                if (id>0)
+                {
+                    ModelState.Clear();
+                    ViewBag.IsSuccess = "Data Added";
+                }
+            }
+            return View();
+        }
+        //Display alll The Records At at time
+        public ActionResult GetAllRecords()
+        {
+            var result = repository.GetAllEmployees();
+            return View(result);
+        }
+        //Display the reords single records at a time
+        public ActionResult Details(int id)
+        {
+            var employee = repository.GetEmployee(id);
+            return View(employee);
+        }
+        //update Action performs
+        public ActionResult Edit(int id)
+        {
+            var employee = repository.GetEmployee(id);
+            return View(employee);
+        }
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.UpDateEmployee(model.Id, model);
+                return RedirectToAction("GetAllRecords");
+            }
+            return View();
+        }
+
+        //Delete the records from database
+        
+        public ActionResult Delete(int id)
+        {
+            repository.DeleteEmployee(id);
+            return RedirectToAction("GetAllRecords");
+        }
+    }
+}
---
+++ MyAppDb/DbOperatuon/EmployeeRepository.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyAppModels;
+
+namespace MyAppDb.DbOperatuon
+{
+   public class EmployeeRepository
+    {
+        public int AddEmployee(EmployeeModel model)
+        {
+            using (var context=new EmployeeDBEntities())
+            {
+                //Insert the In Database
+                Employee Emp = new Employee()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    Code = model.Code
+                };
+                if (model.Address!=null)
+                {
+                    Emp.Address = new Address()
+                    {
+                        Details=model.Address.Details,
+                        Country=model.Address.Country,
+                        State=model.Address.State
+                    };
+                }
+                context.Employee.Add(Emp);
+                context.SaveChanges();
+                return Emp.Id;
+            }
+        }
+        //Display the data from database
+        //get All the Records From The Dat Base
+        public List<EmployeeModel> GetAllEmployees()
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                var result = context.Employee
+                    .Select(a=>new EmployeeModel()
+                {
+                 Id=a.Id,
+                 AddressId=a.AddressId,
+                 Code=a.Code,
+                 Email=a.Email,
+                 FirstName=a.FirstName,
+                 LastName=a.LastName,
+                  Address=new AddressModel()
+                  {
+                     Id=a.Address.Id,
+                     Details=a.Address.Details,
+                     Country=a.Address.Country,
+                     State=a.Address.State
+                  }
+                }).Take(5).ToList();
+                return result;
+            }
+
+        }
+
+        //Display the data from database single data  
+        //get single  Records From The Dat Base
+        public EmployeeModel GetEmployee(int id)
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                var result = context.Employee.
+Where(a => a.Id == id)
+                    .Select(a => new EmployeeModel()
+                    {
+                        Id = a.Id,
+                        AddressId = a.AddressId,
+                        Code = a.Code,
+                        Email = a.Email,
+                        FirstName = a.FirstName,
+                        LastName = a.LastName,
+                        Address = new AddressModel()
+                        {
+                            Id = a.Address.Id,
+                            Details = a.Address.Details,
+                            Country = a.Address.Country,
+                            State = a.Address.State
+                        }
+                    }).FirstOrDefault();
+                return result;
+            }
+
+        }
+        //Update the databse first method 
+      /*  public bool UpDateEmployee(int id,EmployeeModel model)
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                var employee = context.Employee.FirstOrDefault(a => a.Id == id);
+                if (employee !=null)
+                {
+                    employee.FirstName = model.FirstName;
+                    employee.LastName = model.LastName;
+                    employee.Email = model.Email;
+                    employee.Code = model.Code;
+                }
+                context.SaveChanges();
+                return true;
+            }
+        }*/
+
+        //Update the databse second method 
+        public bool UpDateEmployee(int id, EmployeeModel model)
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                var employee = new Employee();
+                if (employee != null)
+                {
+                    employee.Id = model.Id;
+                    employee.FirstName = model.FirstName;
+                    employee.LastName = model.LastName;
+                    employee.Email = model.Email;
+                    employee.Code = model.Code;
+                    employee.AddressId = model.AddressId;
+                }
+                context.Entry(employee).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+                return true;
+            }
+        }
+
+        //Delete records from database the databse first method
+        /*public bool DeleteEmployee(int id)
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                var employee = context.Employee.FirstOrDefault(a => a.Id == id);
+                if (employee != null)
+                {
+                    context.Employee.Remove(employee);
+                    context.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+        }*/
+        //Delete records from database the databse second method
+        public bool DeleteEmployee(int id)
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                var emp = new Employee()
+                {
+                    Id = id
+                };
+                context.Entry(emp).State = System.Data.Entity.EntityState.Deleted;
+                context.SaveChanges();
+                return false;
+            }
+        }
+
+    }
+}
